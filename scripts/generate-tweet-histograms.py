32b3805bced27fdf4f0d8be435bc6b37061c1ffe
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-09-29 23:01:55 -0400

    Generate tweet histograms

---
+++ scripts/generate-tweet-histograms.py
@@ -0,0 +1,75 @@
+"""
+Given the appropriate dataframes, generate a histogram (via scatterplot) that
+ shows each corporations tweet patterns from May 25 to July 25. If the tweet
+ addressed racial injustice, it is marked black. Otherwise it is grey. Each
+ plot is saved as a PNG to the given FIGURE_DIR.
+"""
+
+from os.path import join as pjoin
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date as dt
+import matplotlib as mpl
+mpl.rcParams['figure.dpi']= 300
+
+import matplotlib.units as munits
+import matplotlib.dates as mdates
+import datetime
+
+def generate_company_chart(company, handle, FIGURE_DIR, df_t):
+    df = df_t[(df_t['Corporation'] == company)].copy()
+    df = df[~pd.isna(df['Racial Justice'])]
+    df['date'] = df['Datetime'].dt.date
+
+    # a too-complicated way of getting counts for each day
+    t = []
+    for date in sorted(df['date'].unique()):
+        m = df[df['date'] == date]
+        t.append([date, list(range(1, m.shape[0]+1))])
+    df.sort_values('Datetime', inplace=True)
+    new_col = []
+    for date, l in t:
+        new_col.extend(l)
+    df['count'] = new_col
+    
+    colormap = ['lightgrey' if x == 0 else 'black' for x in pd.Categorical(df['Racial Justice']).codes]
+    max_count = df['count'].max()
+    
+    ax = df.plot.scatter(
+        x='date', y='count', c=colormap, figsize=(10, .15*(max_count+2)),  
+        xlim=(dt(2020, 5, 24), dt(2020, 7, 26)), ylim=(0, max_count+2)
+    )
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    
+    if max_count < 10:
+        ax.set_yticks([])
+    
+    ax.set_title(f"Tweets by @{handle} ({company})")
+    ax.set_ylabel('')
+    ax.set_xlabel('')
+    
+    plt.savefig(pjoin(FIGURE_DIR, f'{company}.png'), bbox_inches='tight')
+    plt.close()
+
+def main():
+    DATA_DIR = '../fortune-100-blm-dataset/data'
+    FIGURE_DIR = '../figures/tweet-histograms'
+
+    converter = mdates.ConciseDateConverter()
+    munits.registry[np.datetime64] = converter
+    munits.registry[datetime.date] = converter
+    munits.registry[datetime.datetime] = converter
+
+    df_c = pd.read_csv(pjoin(DATA_DIR, 'fortune-100.csv')).dropna()
+    df_t = pd.read_csv(pjoin(DATA_DIR, 'fortune-100-tweets.csv'), parse_dates=['Datetime'])
+
+    for i, row in df_c.iterrows():
+        generate_company_chart(row['Corporation'], row['Handle'], FIGURE_DIR, df_t)
+
+if __name__ == '__main__':
+    main()
+
