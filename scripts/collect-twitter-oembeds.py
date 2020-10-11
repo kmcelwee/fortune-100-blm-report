027d9d5c74cde3c301087a6173b3d67b503f34c4
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-10-10 22:26:49 -0400

    Improve error handling and update with new Lowe's data

--- scripts/collect-twitter-oembeds.py
+++ scripts/collect-twitter-oembeds.py
@@ -2,10 +2,12 @@ import requests
 import pandas as pd
 import json
 
+
 def get_oembed(tweet_id):
     r = requests.get('https://publish.twitter.com/oembed', 
                      params={'url': f"https://twitter.com/twitter/status/{tweet_id}" })
     return json.loads(r.text)
+
 
 def collect_twitter_oembeds(HISTOGRAM_JSON_PATH, OEMBED_JSON_PATH, hard=False):
     with open(HISTOGRAM_JSON_PATH) as f:
@@ -19,19 +21,23 @@ def collect_twitter_oembeds(HISTOGRAM_JSON_PATH, OEMBED_JSON_PATH, hard=False):
 
     all_tweet_ids = []
     for comp, comp_dict in histogram_json.items():
-        all_tweet_ids.extend([tweet['ID'] for tweet in comp_dict])
+        all_tweet_ids.extend([tweet['ID'] for tweet in comp_dict['tweets']])
 
+    error_ids = []
     for i, tweet_id in enumerate(all_tweet_ids):
         if i % 100 == 0:
             print(i, end=',')
         if tweet_id not in oembed_json:
             try:
-                oembed_json[tweet_id] = get_oembed(tweet_id)
-            except:
-                if 'error' not in oembed_json:
-                    oembed_json['error'] = [tweet_id]
+                return_json = get_oembed(tweet_id)
+                if 'error' in return_json:
+                    error_ids.append(tweet_id)
                 else:
-                    oembed_json['error'].append(tweet_id)
+                    oembed_json[tweet_id] = return_json
+            except:
+                error_ids.append(tweet_id)
+
+    oembed_json['error'] = error_ids
 
     with open(OEMBED_JSON_PATH, 'w') as f:
         json.dump(oembed_json, f, indent=4)
