dd600199e66c95682848eee138302717b88ea4c8
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-10-28 12:50:47 -0400

    Fix refresh exception

--- site/js/header.js
+++ site/js/header.js
@@ -1,10 +1,12 @@
+function fadeTitleCard() {
+  if (window.scrollY != 0) {
+      $('.header').css({'opacity': 1})
+  } else {
+      $('.header').css({'opacity': 0})
+  }
+}
+
 $(document).ready(function() {
-    function fadeTitleCard() {
-      if (window.scrollY != 0) {
-          $('.header').css({'opacity': 1})
-      } else {
-          $('.header').css({'opacity': 0})
-      }
-    }
+    fadeTitleCard();
     window.onscroll = function() {fadeTitleCard()};
 })
