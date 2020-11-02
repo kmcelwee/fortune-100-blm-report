d51cfc1931ebf5c4432771d71fecdc85e394fe27
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-11-02 08:49:39 -0500

    Create header, explore sidenote functionality, and rename from company to corporation

---
+++ site/js/report.js
@@ -0,0 +1,14 @@
+function toggleSidenote() {
+    // $('.sidenote').css({
+    //     'height': 'inherit',
+    //     'display': 'flex'
+    // })
+    $('.sidenote').toggleClass('sidenote-toggle');
+    $('.arrow').toggleClass('arrow-rotate')
+    // $('.arrow').css({
+    //     'transform': 'rotate(20deg)'
+    // })
+}
+$(document).ready({
+    
+});
