432e861120917b7156c3b6eacb1c20ea0ab05484
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-11-06 10:41:39 -0500

    Add signal to scroll up to view background

--- site/js/index.js
+++ site/js/index.js
@@ -3,12 +3,14 @@ function fadeTitleCard() {
       $('.header').css({'opacity': 1})
   } else {
       $('.header').css({'opacity': 0})
+      $('#scroll_up').fadeOut()
   }
 }
 
 window.onscroll = fadeTitleCard;
 
 $(document).ready(function() {
-    $('html, body').animate({scrollTop: 2}, 500);
+    $('html, body').animate({scrollTop: 1}, 500);
     fadeTitleCard();
+    $('#scroll_up').delay(2000).fadeIn()
 })
