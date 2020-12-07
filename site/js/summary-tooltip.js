3b37c91c5ded6d038273b51bdb35422e70fd731e
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-12-06 20:15:41 -0500

    Experiment with histogram tooltip

---
+++ site/js/summary-tooltip.js
@@ -0,0 +1,6 @@
+var m ;
+$(document).ready(function() {
+    $('.summary-tooltip-container').click(function() {
+        $(this).find('.summary-tooltip-tooltip').css({'display': 'inline-block'})
+    })
+});
