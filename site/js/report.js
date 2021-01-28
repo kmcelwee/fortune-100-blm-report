ce312511d1d3d1166811d46516b08f85b9e3e105
Kevin McElwee <kevinrmcelwee@gmail.com>
2021-01-28 09:06:34 -0500

    Generalize links

--- site/js/report.js
+++ site/js/report.js
@@ -6,7 +6,7 @@ $.getJSON("https://raw.githubusercontent.com/kmcelwee/fortune-100-blm-report/mai
         var handle = corp2handle[$(this).text()];
         if (!handle) {handle = $(this).attr('handle')}
         if (!handle) {console.log('PROBLEM!'); console.log($(this).text())}
-        $(this).attr('href', `https://kmcelwee.github.io/fortune-100-blm-report/site/corporate-summaries.html#${handle}`)
+        $(this).attr('href', `corporate-summaries.html#${handle}`)
         $(this).attr('target', '_blank')
         $(this).append(`<img src="img/icon-histogram-2.png" alt="histogram icon" class="histogram_icon">`)
     })
